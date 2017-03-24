1ef3c9d9ba211b4b5965d29e239fdf0ea080a491
Khaled Mohammed <khaled.mak94@gmail.com>
2017-03-25 02:07:00 +0530

    added functions to support the changes in single.php

--- templates/elements-2.php
+++ templates/elements-2.php
@@ -294,3 +294,167 @@ if ( !function_exists( 'ampforwp_the_head' ) ) {
     ampforwp_the_css( $amp_post_template_object );
   }
 }
+
+
+
+// # Util Function
+// All Head Functions Combined into one
+if ( !function_exists( 'ampforwp_comments_pagination' ) ) {
+  function ampforwp_comments_pagination( $id ) {
+        global $redux_builder_amp;
+        $data = get_option( 'ampforwp_design' );
+        $enable_comments = false;
+
+        if ($data['elements'] == '') {
+          $data['elements'] = "meta_info:1,title:1,featured_image:1,content:1,meta_taxonomy:1,social_icons:1,comments:1,related_posts:1";
+        }
+        if( isset( $data['elements'] ) || ! empty( $data['elements'] ) ){
+          $options = explode( ',', $data['elements'] );
+        };
+        if ($options): foreach ($options as $key=>$value) {
+          switch ($value) {
+              case 'comments:1':
+                $enable_comments = true;
+              break;
+          }
+        } endif;
+      if ( $enable_comments ) { ?>
+          <?php
+            $postID = $id;
+            $comments = get_comments(array(
+                'post_id' => $postID,
+                'status' => 'approve' //Change this to the type of comments to be displayed
+            ));
+          if ( $comments ) { ?>
+            <div class="ampforwp-comment-wrapper">
+              <div class="amp-wp-content comments_list">
+                  <h3><?php echo $redux_builder_amp['amp-translator-view-comments-text'] ?></h3>
+                  <ul>
+                  <?php
+                  $page = (get_query_var('page')) ? get_query_var('page') : 1;
+                  $total_comments = get_comments( array(
+                    'orderby' 	=> 'post_date' ,
+                    'order' 	=> 'DESC',
+                    'post_id'	=> $postID,
+                    'status' 	=> 'approve',
+                    'parent'	=>0 )
+                  );
+                  $pages = ceil(count($total_comments)/AMPFORWP_COMMENTS_PER_PAGE);
+                    $pagination_args = array(
+                    'base'         =>  @add_query_arg('page','%#%'),
+                    'format'       => '?page=%#%',
+                    'total'        => $pages,
+                    'current'      => $page,
+                    'show_all'     => False,
+                    'end_size'     => 1,
+                    'mid_size'     => 2,
+                    'prev_next'    => True,
+                    'prev_text'    => $redux_builder_amp['amp-translator-previous-text'],
+                    'next_text'    => $redux_builder_amp['amp-translator-next-text'],
+                    'type'         => 'plain'
+                  );
+
+                  // Display the list of comments
+                  function ampforwp_custom_translated_comment_paginated($comment, $args, $depth){
+                    $GLOBALS['comment'] = $comment;
+                    global $redux_builder_amp; ?>
+                    <li id="li-comment-<?php comment_ID() ?>"
+                    <?php comment_class(); ?> >
+                      <article id="comment-<?php comment_ID(); ?>" class="comment-body">
+                        <footer class="comment-meta">
+                          <div class="comment-author vcard">
+                            <?php
+                            printf(__('<b class="fn">%s</b> <span class="says">'.$redux_builder_amp['amp-translator-says-text'].':</span>'), get_comment_author_link()) ?>
+                          </div>
+                          <!-- .comment-author -->
+                          <div class="comment-metadata">
+                            <a href="<?php echo htmlspecialchars( trailingslashit( get_comment_link( $comment->comment_ID ) ) ) ?>">
+                              <?php
+                              printf(__('%1$s '.$redux_builder_amp['amp-translator-at-text'].' %2$s'), get_comment_date(),  get_comment_time())
+                              ?>
+                            </a>
+                            <?php edit_comment_link(__('('.$redux_builder_amp['amp-translator-Edit-text'].')'),'  ','') ?>
+                          </div>
+                          <!-- .comment-metadata -->
+                        </footer>
+                          <!-- .comment-meta -->
+                        <div class="comment-content">
+                            <p><?php
+                              // $pattern = "~[^a-zA-Z0-9_ !@#$%^&*();\\\/|<>\"'+.,:?=-]~";
+                              $emoji_content = get_comment_text();
+                              $emoji_content = ampforwp_sanitize_html_to_amphtml( $emoji_content );
+                              // $emoji_free_comments = preg_replace($pattern,'',$emoji_content);
+                              echo $emoji_content; ?>
+                            </p>
+                        </div>
+                          <!-- .comment-content -->
+                      </article>
+                     <!-- .comment-body -->
+                    </li>
+                  <!-- #comment-## -->
+                    <?php
+                  }// end of ampforwp_custom_translated_comment_paginated()
+                  wp_list_comments( array(
+                    'per_page' 			=> AMPFORWP_COMMENTS_PER_PAGE, //Allow comment pagination
+                    'page'              	=> $page,
+                    'style' 				=> 'li',
+                    'type'				=> 'comment',
+                    'max_depth'   		=> 5,
+                    'avatar_size'			=> 0,
+                    'callback'				=> 'ampforwp_custom_translated_comment_paginated',
+                    'reverse_top_level' 	=> false //Show the latest comments at the top of the list
+                  ), $comments);
+                  echo paginate_links( $pagination_args );?>
+                  </ul>
+              </div>
+              <div class="comment-button-wrapper">
+                  <a href="<?php echo get_permalink().'?nonamp=1'.'#commentform' ?>" rel="nofollow"><?php esc_html_e( $redux_builder_amp['amp-translator-leave-a-comment-text']  ); ?></a>
+              </div>
+            </div><?php
+          } else {
+              if ( !comments_open() ) {
+                // Dont do Anything
+              } else { ?>
+                <div class="ampforwp-comment-wrapper">
+                  <div class="comment-button-wrapper">
+                     <a href="<?php echo get_permalink().'?nonamp=1'.'#commentform'  ?>" rel="nofollow"><?php esc_html_e( $redux_builder_amp['amp-translator-leave-a-comment-text']  ); ?></a>
+                  </div>
+                </div>
+          <?php }
+          } ?>
+
+        </div><?php
+      }
+  }
+}
+
+
+// # Util FUnction
+// Echo Social Share Icons
+if( !function_exists( 'ampforwp_the_social_share' ) ){
+  function ampforwp_the_social_share() {
+    global $redux_builder_amp;
+    if($redux_builder_amp['enable-single-social-icons'] == true)  { ?>
+      <div class="sticky_social">
+        <?php if($redux_builder_amp['enable-single-facebook-share'] == true)  { ?>
+            <amp-social-share type="facebook"   width="50" height="28"></amp-social-share>
+          <?php } ?>
+          <?php if($redux_builder_amp['enable-single-twitter-share'] == true)  { ?>
+            <amp-social-share type="twitter"    width="50" height="28"></amp-social-share>
+          <?php } ?>
+          <?php if($redux_builder_amp['enable-single-gplus-share'] == true)  { ?>
+            <amp-social-share type="gplus"      width="50" height="28"></amp-social-share>
+          <?php } ?>
+          <?php if($redux_builder_amp['enable-single-email-share'] == true)  { ?>
+            <amp-social-share type="email"      width="50" height="28"></amp-social-share>
+          <?php } ?>
+          <?php if($redux_builder_amp['enable-single-pinterest-share'] == true)  { ?>
+            <amp-social-share type="pinterest"  width="50" height="28"></amp-social-share>
+          <?php } ?>
+          <?php if($redux_builder_amp['enable-single-linkedin-share'] == true)  { ?>
+            <amp-social-share type="linkedin"   width="50" height="28"></amp-social-share>
+          <?php } ?>
+      </div>
+    <?php }
+  }
+}

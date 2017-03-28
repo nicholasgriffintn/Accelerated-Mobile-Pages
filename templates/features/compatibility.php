4ee073df73f71b42154d5b127d08ef8d7785431e
Khaled Mohammed <khaled.mak94@gmail.com>
2017-03-28 19:57:21 +0530

    moved compatibility code to compatibility.php

---
+++ templates/features/compatibility.php
@@ -0,0 +1,9 @@
+<?php
+//51. Adding Digg Digg compatibility with AMP
+function ampforwp_dd_exclude_from_amp() {
+if(ampforwp_is_amp_endpoint()) {
+    remove_filter('the_excerpt', 'dd_hook_wp_content');
+    remove_filter('the_content', 'dd_hook_wp_content');
+	}
+}
+add_action('template_redirect', 'ampforwp_dd_exclude_from_amp');

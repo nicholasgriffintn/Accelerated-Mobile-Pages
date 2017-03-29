cee07587e168cf2ce5e2a940535cfbffdbf7aee4
Khaled Mohammed <khaled.mak94@gmail.com>
2017-03-30 00:36:20 +0530

    Merged all Scripts code into a single function

--- templates/features/scripts.php
+++ templates/features/scripts.php
@@ -18,35 +18,13 @@ function ampforwp_add_ads_scripts( $data ) {
 			$data['amp_component_scripts']['amp-ad'] = AMPFORWP_AMP_AD_SCRIPT;
 		}
 	}
-	return $data;
-}
 
-
-// 50. Properly adding noditification Scritps the AMP way
-add_filter( 'amp_post_template_data', 'ampforwp_add_notification_scripts' );
-function ampforwp_add_notification_scripts( $data ) {
-	global $redux_builder_amp;
 	if ( $redux_builder_amp['amp-enable-notifications'] == true ) {
 		if ( empty( $data['amp_component_scripts']['amp-user-notification'] ) ) {
 			$data['amp_component_scripts']['amp-user-notification'] = AMPFORWP_NOTIFICATIONS_SCRIPT;
 		}
 	}
-	return $data;
-}
 
-
-// 48. Remove all unwanted scripts on search pages
-add_filter( 'amp_post_template_data', 'ampforwp_remove_scripts_search_page' );
-function ampforwp_remove_scripts_search_page( $data ) {
-	if( is_search() ) {
-		// Remove all unwanted scripts on search pages
-		unset( $data['amp_component_scripts']);
-	}
-	return $data;
-}
-
-add_filter( 'amp_post_template_data', 'ampforwp_add_lightbox_and_form_scripts');
-function ampforwp_add_lightbox_and_form_scripts( $data ) {
 	if ( ampforwp_is_search_enabled() ) {
 		global $redux_builder_amp;
 		// Add Scripts only when Search is Enabled
@@ -59,12 +37,7 @@ function ampforwp_add_lightbox_and_form_scripts( $data ) {
 			}
 		}
 	}
-	return $data;
-}
 
-// Add required Javascripts for Design 3
-add_filter( 'amp_post_template_data', 'ampforwp_add_design3_required_scripts', 100 );
-function ampforwp_add_design3_required_scripts( $data ) {
 	if( ampforwp_design_selector() == 3) {
 		global $redux_builder_amp;
 		$amp_menu_has_child = get_transient( 'ampforwp_has_nav_child' );
@@ -83,25 +56,7 @@ function ampforwp_add_design3_required_scripts( $data ) {
 			}
 		}
 	}
-	return $data;
-}
 
-
-//	add_action('amp_post_template_head','ampforwp_register_social_sharing_script');
-function ampforwp_register_social_sharing_script( $data ) {
-	if( is_socialshare_or_socialsticky_enabled_in_ampforwp() ) {
-		if ( empty( $data['amp_component_scripts']['amp-social-share'] ) ) {
-			$data['amp_component_scripts']['amp-social-share'] = AMPFORWP_SOCIAL_SHARE_SCRIPT;
-		}
- 	}
-	return $data;
-}
-
-
-// 6. Add required Javascripts for extra AMP features
-add_filter('amp_post_template_data','ampforwp_register_additional_scripts', 20);
-function ampforwp_register_additional_scripts( $data ) {
-	global $redux_builder_amp;
 	if( is_page() ) {
 		if ( empty( $data['amp_component_scripts']['amp-form'] ) ) {
 			$data['amp_component_scripts']['amp-form'] = AMPFORWP_FORM_SCRIPT;
@@ -129,34 +84,16 @@ function ampforwp_register_additional_scripts( $data ) {
 			}
 		}
 	}
-	return $data;
-}
 
-
-// 6.1 Adding Analytics Scripts
-add_filter('amp_post_template_data','ampforwp_register_analytics_script', 20);
-function ampforwp_register_analytics_script( $data ){
 	if ( empty( $data['amp_component_scripts']['amp-analytics'] ) ) {
 		$data['amp_component_scripts']['amp-analytics'] = AMPFORWP_ANALYTICS_SCRIPT;
 	}
-	return $data;
-}
 
-
-add_filter( 'amp_post_template_data', 'ampforwp_add_amp_related_scripts', 20 );
-function ampforwp_add_amp_related_scripts( $data ) {
-	global $redux_builder_amp;
 	// Adding Sidebar Script
 	if ( empty( $data['amp_component_scripts']['amp-sidebar'] ) ) {
 		$data['amp_component_scripts']['amp-sidebar'] = AMPFORWP_SIDE_BAR_SCRIPT;
 	}
-	return $data;
-}
 
-
-add_filter( 'amp_post_template_data', 'ampforwp_add_disqus_scripts' );
-function ampforwp_add_disqus_scripts( $data ) {
-	global $redux_builder_amp;
 	if ( $redux_builder_amp['ampforwp-disqus-comments-support'] && is_singular() ) {
 		if( $redux_builder_amp['ampforwp-disqus-comments-name'] !== '' ) {
 			if ( empty( $data['amp_component_scripts']['amp-iframe'] ) ) {
@@ -166,6 +103,11 @@ function ampforwp_add_disqus_scripts( $data ) {
 	}
 	// remove direction attribute from the AMP HTMl #541
 	unset( $data['html_tag_attributes']['dir'] );
+
+	if( is_search() ) {
+		// Remove all unwanted scripts on search pages
+		unset( $data['amp_component_scripts']);
+	}
 	return $data;
 }
 

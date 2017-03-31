1fcee38b1474ce1603798ee18b990ffb1c68574a
Khaled Mohammed <khaled.mak94@gmail.com>
2017-03-31 14:50:55 +0530

    analytics moved to its own file

--- templates/constants.php
+++ templates/constants.php
@@ -32,6 +32,7 @@ define('AMPFORWP_STRUCTURED_DATA_FILE', AMPFORWP_PLUGIN_DIR . 'templates/feature
 define('AMPFORWP_SEO_FILE', AMPFORWP_PLUGIN_DIR . 'templates/features/general/seo.php');
 define('AMPFORWP_TITLE_FILE', AMPFORWP_PLUGIN_DIR . 'templates/features/general/title.php');
 define('AMPFORWP_DESIGN_SPECIFIC_FUNCTIONS', AMPFORWP_PLUGIN_DIR . 'templates/features/general/design-specific-functions.php');
+define('AMPFORWP_ANALYTICS_FILE', AMPFORWP_PLUGIN_DIR . 'templates/features/general/analytics.php');
 define('AMPFORWP_DISQUS_HOST', "https://ampforwp.appspot.com/?api=". AMPFORWP_DISQUS_URL);
 
 
